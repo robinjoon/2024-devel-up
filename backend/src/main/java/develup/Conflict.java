5abb24bbc64bf41f9b25a8af5ec2b9ab52206185
임수빈 <robin980108@naver.com>
2024-07-20 18:33:59 +0900

    Conflict 클래스를 수정하고 사용한 코드 커밋 (#3)

--- backend/src/main/java/develup/Conflict.java
+++ backend/src/main/java/develup/Conflict.java
@@ -2,8 +2,10 @@ package develup;
 
 public class Conflict {
     int id;
+    String name;
 
-    public Conflict(int id) {
+    public Conflict(int id, String name) {
         this.id = id;
+        this.name = name;
     }
 }
